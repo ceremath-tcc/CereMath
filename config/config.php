b5a1ff7f9bf9c4106b08758d50e8f890d47850d6
ceremath-tcc <ceremath.tcc@gmail.com>
2025-11-07 17:26:47 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,9 @@
 <?php
     //config.php
     return [
-        'host' => 'localhost',
-        'user' => 'root',
-        'pass' => 'aluno123',
-        'name' => 'Ceremath'
+        'host' = 'ceremath021-1-ceremath-615f.f.aivencloud.com';
+        'port' = 10861;
+        'dbname' = 'defaultdb';
+        'user' = 'avnadmin';
+        'pass' = 'AVNS_wQkr-6YhgBAjELwj-Fs';
     ];
