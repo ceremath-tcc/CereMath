138eba26282e89fe81bb5dbccd7e49deb4170290
ceremath-tcc <ceremath.tcc@gmail.com>
2025-11-07 14:55:28 -0300

    Update login.php

--- public/login.php
+++ public/login.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+    require_once './account/login-prohibited.php'
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-    <?php require_once './components/fonts.php'; require_once './account/login-prohibited.php' ?>
+    <?php require_once './components/fonts.php'; ?>
     <div class="container">
        
         <canvas id="background"></canvas> 
