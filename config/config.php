135d13abdffcc251bbf0d461ea7ca79d2912a821
ceremath-tcc <ceremath.tcc@gmail.com>
2025-11-07 17:32:06 -0300

    integrando a base de dados Aiven - 1

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
     //config.php
     return [
-        'host' = 'ceremath021-1-ceremath-615f.f.aivencloud.com';
-        'port' = 10861;
-        'dbname' = 'defaultdb';
-        'user' = 'avnadmin';
-        'pass' = 'AVNS_wQkr-6YhgBAjELwj-Fs';
+        'host' => 'ceremath021-1-ceremath-615f.f.aivencloud.com';
+        'port' => 10861;
+        'dbname' => 'defaultdb';
+        'user' => 'avnadmin';
+        'pass' => 'AVNS_wQkr-6YhgBAjELwj-Fs';
     ];
