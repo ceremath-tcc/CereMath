2f8b0d92abd898e6e20c7dbd7a92147ce6b21ed1
ceremath-tcc <ceremath.tcc@gmail.com>
2025-11-12 15:32:17 -0300

    Update exec_login.php

--- public/account/exec_login.php
+++ public/account/exec_login.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../../src/controller/loginController.php';
 require '../../vendor/autoload.php'; // PHPMailer
 
+
 date_default_timezone_set('America/Sao_Paulo');
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -30,28 +31,29 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     //Inicio de enviar o e-mail para a conta
 
     // ===== Envia o e-mail =====
-    $mail = new PHPMailer(true);
+    $apiKey = getenv('RESEND_API_KEY');
+    
+    $data = [
+      "from" => "onboarding@resend.dev",
+      "to" => ["usuario@example.com"],
+      "subject" => "Novo login detectado! - Ceremath",
+      "html" => "Login detectado na sua conta: <b> " . $_SESSION['username'] . "</b> no dia " . date('d/m/Y') . " às " . date('H:i:s') . " - Horario de Brasília.<br><br>Caso não se recorde de ter feito o login, é recomendavel que mude sua senha imediatamente.</p>"
+    ];
+    
+    $ch = curl_init("https://api.resend.com/emails");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      "Authorization: Bearer $apiKey",
+      "Content-Type: application/json"
+    ]);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    
+    $response = curl_exec($ch);
+    curl_close($ch);
+    
+    echo $response;
 
-    try {
-        $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'ceremath.tcc@gmail.com'; 
-        $mail->Password   = 'syiu bsra encp tzmw'; // senha de aplicativo
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = 587;
-
-        $mail->setFrom('ceremath.tcc@gmail.com', 'Ceremath');
-        $mail->addAddress($_SESSION['email'], $_SESSION['username']); 
-
-        $mail->isHTML(true);
-        $mail->Subject = 'Novo login detectado! - Ceremath';
-        $mail->Body    = "Login detectado na sua conta: <b> " . $_SESSION['username'] . "</b> no dia " . date('d/m/Y') . " às " . date('H:i:s') . " - Horario de Brasília.<br><br>Caso não se recorde de ter feito o login, é recomendavel que mude sua senha imediatamente.";
-
-        $mail->send();
-    } catch (Exception $e) {
-        error_log("Erro ao enviar email: {$mail->ErrorInfo}");
-    }
     //Final E-mail
 
     unset($controller);
