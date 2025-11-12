8266ef25a58ee721b10a14a6186de91b88768e79
ceremath-tcc <ceremath.tcc@gmail.com>
2025-11-12 13:56:14 -0300

    Update sidebar.php

--- public/components/sidebar.php
+++ public/components/sidebar.php
@@ -1,5 +1,7 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-file_get_contents(__DIR__ . '/load-questoes.php?qtd=7&materia=1');
+$json = file_get_contents('https://ceremath.onrender.com/public/components/load-questoes.php?qtd=7&materia=1');
+
+$dados = json_decode($json, true);
 echo $dados;
