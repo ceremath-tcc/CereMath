265f038a6bb2ac193bd0bde717cd3050484b42b0
ceremath-tcc <ceremath.tcc@gmail.com>
2025-11-12 13:46:17 -0300

    Update sidebar.php

--- public/components/sidebar.php
+++ public/components/sidebar.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-$dados = file_get_contents('components/load-questoes.php?qtd=7&materia=1');
+$dados = file_get_contents('load-questoes.php?qtd=7&materia=1');
 echo $dados;
