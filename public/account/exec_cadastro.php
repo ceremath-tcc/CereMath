bea1dccdbfd2fb1df37145dba1ad567fae58dec6
ceremath-tcc <ceremath.tcc@gmail.com>
2025-11-12 15:52:29 -0300

    Update exec_cadastro.php

--- public/account/exec_cadastro.php
+++ public/account/exec_cadastro.php
@@ -33,28 +33,26 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     //Inicio de enviar o e-mail para a conta
 
     // ===== Envia o e-mail =====
-    $mail = new PHPMailer(true);
-
-    try {
-        $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'ceremath.tcc@gmail.com'; 
-        $mail->Password   = 'syiu bsra encp tzmw'; // senha de aplicativo
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = 587;
-
-        $mail->setFrom('ceremath.tcc@gmail.com', 'Ceremath');
-        $mail->addAddress($_SESSION['email'], $_SESSION['username']); 
-
-        $mail->isHTML(true);
-        $mail->Subject = 'Bem vindo ao Ceremath!';
-        $mail->Body    = "Sua conta <b> " . $_SESSION['username'] . "</b> foi criada com sucesso no dia " . date('d/m/Y') . " às " . date('H:i:s') . " - Horario de Brasília.<br>Seja bem vindo ao nosso site e em nome de todos os participantes do nosso TCC, desejamos que consiga aprender a matematica!<br><br>Caso não se recorde de ter feito o cadastro, é recomendavel que tome ação imediatamente quanto a isso.";
-
-        $mail->send();
-    } catch (Exception $e) {
-        error_log("Erro ao enviar email: {$mail->ErrorInfo}");
-    }
+   $apiKey = getenv('RESEND_API_KEY');
+    
+    $data = [
+      "from" => "onboarding@resend.dev",
+      "to" => ["$_SESSION['email']"],
+      "subject" => "Bem vindo ao Ceremath!",
+      "html" => "Sua conta <b> " . $_SESSION['username'] . "</b> foi criada com sucesso no dia " . date('d/m/Y') . " às " . date('H:i:s') . " - Horario de Brasília.<br>Seja bem vindo ao nosso site e em nome de todos os participantes do nosso TCC, desejamos que consiga aprender a matematica!<br><br>Caso não se recorde de ter feito o cadastro, é recomendavel que tome ação imediatamente quanto a isso."
+    ];
+    
+    $ch = curl_init("https://api.resend.com/emails");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      "Authorization: Bearer $apiKey",
+      "Content-Type: application/json"
+    ]);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    
+    $response = curl_exec($ch);
+    curl_close($ch);
     //Final E-mail
 
     unset($controller);
