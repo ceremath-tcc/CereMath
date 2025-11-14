d1af4e17afd8b37fd4bf27d11e5c6e2347632b8e
ceremath-tcc <ceremath.tcc@gmail.com>
2025-11-14 10:52:51 -0300

    Update studybox.js

--- public/js/studybox.js
+++ public/js/studybox.js
@@ -11,8 +11,25 @@ chatBox.scrollTo({
 //Funcao responsavel por desavilitar o input apos o envio
 const form = document.getElementById("form");
 const input = document.getElementById("valor");
+const btn_re = document.getElementById("btn-refazer");
 
 form.addEventListener("submit", function (e) {
+  if(document.getElementById('loadingSpinner').style.visibility == 'visible'){
+    e.preventDefault(); // ignora novos envios
+    console.log("Ignorado!");
+    return;
+  }
+  // Desabilita o input
+  input.readOnly = true;
+
+  //Mostra o spinner
+  document.getElementById('loadingSpinner').style.visibility = 'visible';
+
+  // Aqui você mandaria a mensagem para o backend...
+  console.log("Mensagem enviada:", input.value);
+});
+
+btn_re.addEventListener("submit", function (e) {
   if(document.getElementById('loadingSpinner').style.visibility == 'visible'){
     e.preventDefault(); // ignora novos envios
     console.log("Ignorado!");
